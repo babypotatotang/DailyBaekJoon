9d38e9ac2025328bb21d09e211a1cba99fade5fd
babypotatotang <gwonso4502@gmail.com>
2022-09-23 10:51:21 +0900

    silver 2: 수정

--- Greedy Algorithm/17503번_맥주축제.py
+++ Greedy Algorithm/17503번_맥주축제.py
@@ -6,13 +6,14 @@ def solve():
 
     n,m,k = map(int,input().split())
     beer = [ list(map(int,input().split())) for _ in range(k)]
-    beer.sort(key = lambda x: x[1]) # 도수를 기준으로 오름차순 정렬
+    beer.sort(key = lambda x: x[1]) # 도수를 기준으로 오름차순 정렬 -> 작은 도수부터 채워나가므로 간 레벨의 최소값을 출력할 수 있음 
 
     likes = []; hap = 0
     for i in beer:
         if len(likes) < n: # 아직 더 들어가야한다면 
             heapq.heappush(likes, i) # push
             hap+= i[0] # 선호도 합 
+
             if len(likes) == n: # n의 개수를 채웠다면
                 if hap >= m: # m보다 크거나 같을 경우 조건 만족 
                     return i[1] # 가장 큰 도수 반환
