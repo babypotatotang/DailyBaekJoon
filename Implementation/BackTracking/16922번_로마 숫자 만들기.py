31ff65d0b888de92d72e362d7ce982d4e918d36a
babypotatotang <gwonso4502@gmail.com>
2022-09-29 10:16:51 +0900

    silver 4 - edit file name

--- Implementation/BackTracking/16922번_로마 숫자 만들기.py
+++ Implementation/BackTracking/16922번_로마 숫자 만들기.py
@@ -1,6 +1,6 @@
 n = int(input())
 
-nums = [1,5,10,50]
+nums = [1,5,10,50] # 사용할 숫자들 
 result = []
 
 def track(num_list, depth):
@@ -9,7 +9,7 @@ def track(num_list, depth):
         return 
 
     for num in nums:
-        if num >= num_list[-1]:
+        if num >= num_list[-1]: # 크거나 같은 숫자에 대해서 append 
             num_list.append(num)
             track(num_list, depth+1)
             num_list.pop()
@@ -21,5 +21,5 @@ for num in nums:
     track(num_list,1)
     num_list.pop()
 
-result = set(result)
-print(len(result))    
+result = set(result) # 중복 제거 후
+print(len(result))   # 결과 개수 반환 
