600158f4cd6c5fd4fab434fd56c4dd2a29623a1c
babypotatotang <gwonso4502@gmail.com>
2022-10-08 11:12:56 +0900

    silver 2

---
+++ Implementation/Math/1735번_분수 합.py
@@ -0,0 +1,22 @@
+import math 
+
+a1, b1 = map(int,input().split())
+a2, b2 = map(int,input().split())
+
+# 최소공배수 찾기 
+lcm = math.lcm(b1,b2)
+
+# 1과 2에 대한 최소공배수가 되기 위한 몫 구하기
+div1 = lcm / b1; div2 = lcm / b2
+
+# 각 몫을 곱해주기
+aresult = int(a1*div1 + a2 * div2)
+bresult = lcm 
+
+# 최대 공약수 찾기 
+gcd = math.gcd(aresult, bresult)
+
+# 기약분수 만들기 
+aresult //= gcd; bresult //=gcd
+
+print(f"{aresult} {bresult}")
