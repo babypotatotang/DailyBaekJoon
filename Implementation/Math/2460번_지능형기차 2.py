48f2bb34142be8aef85a6920546cf532a554a2c4
babypotatotang <gwonso4502@gmail.com>
2022-11-01 09:25:07 +0900

    bronze 3

---
+++ Implementation/Math/2460번_지능형기차 2.py
@@ -0,0 +1,10 @@
+people = [0] * 10
+
+for i in range(10): 
+    t_out, t_in, = map(int, input().split())
+
+    if i == 0 : people[0] = t_in
+    else:
+        people[i] = people[i-1] - t_out + t_in
+
+print(people)
